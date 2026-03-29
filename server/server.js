4ab34838a4ddf3880fbb08963af0c49e21386888
jairsnieto-bit <jairsnieto@gmail.com>
2026-03-28 22:01:02 -0500

    Comment out createUploadsDirectory call
    Comment out the createUploadsDirectory function call.

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Create uploads directory
-createUploadsDirectory();
+//createUploadsDirectory();
 
 // Serve uploaded files
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
