b95efa423757c1baa5e3fd1a511ef9513e9db890
jairsnieto-bit <jairsnieto@gmail.com>
2026-03-30 15:46:23 -0500

    Refactor Vite config to add server and preview options
    Reorganized Vite configuration to include server and preview settings.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+/*import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
@@ -10,6 +10,30 @@ export default defineConfig({
     port: 4173,
     host: true
   },
+  build: {
+    outDir: 'dist',
+    sourcemap: false
+  }
+})*/
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+
+  server: {
+    port: 3000,
+    host: true // 👈 recomendado también
+  },
+
+  preview: {
+    port: 4173,
+    host: true,
+    allowedHosts: [
+      'mekaplus-frontend.up.railway.app'
+    ]
+  },
+
   build: {
     outDir: 'dist',
     sourcemap: false
